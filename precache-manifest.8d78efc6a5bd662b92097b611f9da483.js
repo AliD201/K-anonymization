2e244df822a465e77e97bbccdb44ef6a645d84c8
AliD201 <AliD201@users.noreply.github.com>
2020-10-08 17:14:54 +0300

    Updates

---
+++ precache-manifest.8d78efc6a5bd662b92097b611f9da483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dad42b3dc7db23a61edda5ebdb1f6bd",
+    "url": "/K-anonymization/index.html"
+  },
+  {
+    "revision": "837e65ef2a1f9e4cd532",
+    "url": "/K-anonymization/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5d21acf96f01d21ff240",
+    "url": "/K-anonymization/static/css/main.97495dc6.chunk.css"
+  },
+  {
+    "revision": "837e65ef2a1f9e4cd532",
+    "url": "/K-anonymization/static/js/2.c929c513.chunk.js"
+  },
+  {
+    "revision": "8d033527bfde269db1e0fb1aca3c57e8",
+    "url": "/K-anonymization/static/js/2.c929c513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d21acf96f01d21ff240",
+    "url": "/K-anonymization/static/js/main.392ea71d.chunk.js"
+  },
+  {
+    "revision": "ec70e83be9b6f84ca417",
+    "url": "/K-anonymization/static/js/runtime-main.fbe4d447.js"
+  }
+]);
